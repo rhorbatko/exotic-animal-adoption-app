ff6f8fb45bf158d82c509e28da50e709b2f850a3
jordaninbold <jordaninbold@gmail.com>
2022-03-22 11:09:33 -0400

    Completed API router and PetType model

--- server/routes/rootRouter.js
+++ server/routes/rootRouter.js
@@ -1,7 +1,14 @@
 import express from "express"
 import clientRouter from "./clientRouter.js"
+import petTypesRouter from "./api/v1/petTypesRouter.js"
 
 const rootRouter = new express.Router()
+
+rootRouter.use("/api/v1/pet-types", petTypesRouter)
+
+rootRouter.get("/", (req, res) => {
+    res.redirect("/pet-types")
+})
 
 rootRouter.use("/", clientRouter)
 
