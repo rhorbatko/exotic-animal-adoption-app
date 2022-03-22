a9ab5705cdb3781276671e67ce531ce13816b8d7
rhorbatko <rghorbatko@gmail.com>
2022-03-22 14:35:18 -0400

    added seeder

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -7,4 +7,17 @@ CREATE TABLE pet_types (
     description TEXT
 );
 
+DROP TABLE IF EXISTS pets;
+CREATE TABLE pets(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    img_url VARCHAR(255) NOT NULL,
+    age INTEGER,
+    vaccination_status BOOLEAN DEFAULT false,
+    adoption_story TEXT NOT NULL,
+    available_for_adoption BOOLEAN DEFAULT true,
+    pet_type_id INTEGER REFERENCES pet_types(id)
+);
+
+
 
--- server/routes/api/v1/petTypesRouter.js
+++ server/routes/api/v1/petTypesRouter.js
@@ -5,14 +5,23 @@ import PetType from "../../../models/PetType.js"
 const petTypesRouter = new express.Router()
 
 petTypesRouter.get("/", async (req, res) => {
-    try {
-        const petTypes = await PetType.findAll()
-        res.status(200).json({ petTypes })
-    } catch (error) {
-        console.error(error)
-        res.status(500).json({ errors: error })
-    }
+  try {
+    const petTypes = await PetType.findAll()
+    res.status(200).json({ petTypes })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ errors: error })
+  }
 })
 
+petTypesRouter.get("/:id", async (req, res) => {
+  try {
+    const petType = await PetType.findById(req.params.id)
+    res.status(200).json({ petType })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ errors: error })
+  }
+})
 
 export default petTypesRouter
