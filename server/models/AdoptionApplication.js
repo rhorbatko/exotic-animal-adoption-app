6e845f62c74e32e1ce908f311248c0db770d4351
rhorbatko <rghorbatko@gmail.com>
2022-03-24 22:53:14 -0400

    added Error List, error validation to form

--- server/models/AdoptionApplication.js
+++ server/models/AdoptionApplication.js
@@ -17,39 +17,40 @@ class AdoptionApplication {
   }
 
   async save() {
-    debugger
-    try{
-      if(!this.name.trim()){
-        throw "Please fill out the name field"
-      }if(!this.phoneNumber.trim()){
-        throw "Please fill out the phone number"
-      }if(!this.email.trim()){
-        throw "Please fill out the email"
-      }if(!this.homeStatus){
-        throw "Please fill out the home status field"
-      }else{
+    const errArray = []
+    if (!this.name.trim()) {
+      errArray.push("Please fill out the name field")
+    }
+    if (!this.phoneNumber.trim()) {
+      errArray.push("Please fill out the phone number")
+    }
+    if (!this.email.trim()) {
+      errArray.push("Please fill out the email")
+    }
+    if (!this.homeStatus) {
+      errArray.push("Please fill out the home status field")
+    }
+    try {
+      if (errArray.length === 0) {
         const query =
-        "INSERT INTO adoption_applications(name, phone_number, email, home_status, application_status, pet_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;"
-      const result = await pool.query(query, [
-        this.name,
-        this.phoneNumber,
-        this.email,
-        this.homeStatus,
-        this.applicationStatus,
-        this.petId
-      ])
+          "INSERT INTO adoption_applications(name, phone_number, email, home_status, application_status, pet_id) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id;"
+        const result = await pool.query(query, [
+          this.name,
+          this.phoneNumber,
+          this.email,
+          this.homeStatus,
+          this.applicationStatus,
+          this.petId
+        ])
 
-      const newApplicationId = result.rows[0].id
-      this.id = newApplicationId
-
-      return true
+        const newApplicationId = result.rows[0].id
+        this.id = newApplicationId
+      } else {
+        throw errArray
       }
-      
-    }catch(error){
-      console.error(error)
+    } catch (error) {
       throw error
     }
-    
   }
 
   async findPet() {
